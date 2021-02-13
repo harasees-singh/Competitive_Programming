6b64b823414d8ea6feba7dbee26b7c9582d04205
Harasees Singh <harasees1202@gmail.com>
2021-02-13 14:16:50 +0530

    XOR SUMS i am close

---
+++ FEBRUARY Long Challenge/SUMXOR2.py
@@ -0,0 +1,74 @@
+'''
+
+You are given a sequence of positive integers A1,A2,…,AN. You should answer Q queries. In each query:
+
+You are given a positive integer M.
+Consider all non-empty subsequences of A with length ≤M. Recall that a subsequence is any sequence that can be created by deleting 
+zero or more elements without changing the order of the remaining elements.
+For each of these subsequences, compute the bitwise XOR of its elements. Your task is to determine the sum of these values. 
+Since this sum can be very large, compute it modulo 998,244,353.
+
+
+combinations('ABCD', 2)
+
+AB AC AD BC BD CD
+
+Note: i am not sure what will happen if for some i and j Ai = Aj.
+will deal with it later
+
+'''
+# here is the base idea for every subsequence when i have to compute it's bitwise xor i will refer to a dictionary 
+# which will already have bitwise xors of the same subsequence but will the last element missing 
+# then i will take that xor value of the subsequence from the dictionary and then do it's bitwise xor with 
+# the last element of the subsequence and then add it to the final sum and also add its xor value as a key value pair in the dictionary
+import array
+import itertools
+
+# arr = [1, 2, 5, 4, 4]
+# for i in itertools.combinations(arr, 2):
+#    print(i)
+
+def SubsequenceMaker(number_of_elements):
+    return tuple(itertools.combinations(elements, number_of_elements))
+
+
+
+computed_xors = {}      # will store all the xors here such that the key is a tuple of numbers (a subsequence in regard to the question) and it's value will be the bitwise xor of all the elements in the tuple
+
+n = int(input())
+elements = tuple(map(int, input().split()))
+
+q = int(input())
+
+sum = 0
+
+for g in range(len(elements)):
+    sum += elements[g]
+
+for p in range(len(elements)):                                                # initializing computed_xors with key values such that each key is a unit length tuple containing an element from elements
+    computed_xors[elements[p]] = elements[p]                                  # and the corres value is the number itself
+
+for i in range(q):
+    query = int(input())
+
+    for j in range(2, query+1):
+        sequence = SubsequenceMaker(j)
+        # print(sequence)
+        for v in range(len(sequence)):
+            if j==2:
+                temp = computed_xors[sequence[0][0]]^sequence[0][1]
+                sum += temp
+                computed_xors[sequence[0]] = temp
+
+                # print(computed_xors)
+            else:
+                temp = computed_xors[sequence[:-1]]^sequence[-1]
+                sum += temp
+                computed_xors[sequence] = temp
+
+    print(sum)
+
+
+
+
+
--- FEBRUARY Long Challenge/TEAMNAME.py
+++ FEBRUARY Long Challenge/TEAMNAME.py
@@ -1,24 +1,18 @@
 # could take funny words in pairs and if they are 'different' then add 2 to the count of good names
+# trying to improve upon the brute force method :)
+
 
 t = int(input())
 for _ in range(t):
     n = int(input())
    
     funny_names = list(map(str, input().split()))
-    funny_names.sort()
-    # print(funny_names)
 
-    # names_after_removing_the_starting_character = [funny_names[i][1:] for i in range(n)]
-    # starting_characters = [funny_names[i][0] for i in range(n)]
+    # could first make potential pairs and then check if after replacing their first letters they happen to be funny or not
 
-    # names_after_removing_the_starting_character = set(names_after_removing_the_starting_character)
-    # names_after_removing_the_starting_character = list(names_after_removing_the_starting_character)
+    # but that was what i did the last time too and it resulted in tle
 
-    count = 0
+    # remember 'hell' and 'test' they did generate 'hest tell' which was a good name as 'hest' and 'tell' were not funny but 'hell' and 'test' were funny
+
     
-    for i in range(n):
-        for j in range(i+1, n):
-            if (funny_names[i][0] != funny_names[j][0]) and (funny_names[i][1:] != funny_names[j][1:]):     # 'different'
-                if funny_names[i][0]+funny_names[j][1:] not in funny_names and funny_names[j][0]+funny_names[i][1:] not in funny_names:
-                    count+=2
-    print(count)
+    
