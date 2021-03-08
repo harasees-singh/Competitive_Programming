b59f35c94c9e09b6353afd0b63cba32841739602
Harasees Singh <harasees1202@gmail.com>
2021-03-09 01:17:26 +0530

    TLE

--- Practice_Problems_codechef/Sorting_by_swaps.py
+++ Practice_Problems_codechef/Sorting_by_swaps.py
@@ -1,57 +1,37 @@
 # do minimum number of adjacent swaps to sort a list 
 # print the minimum swap count
-
-def BubbleSortCustom(li):
-    swaps = 0
-
-    for i in range(len(li)-1):
-        swapped = False
-        for j in range(len(li)-1-i):
-            if li[j]>li[j+1]:
-                li[j], li[j+1] = li[j+1], li[j]
-                swapped = True
-                swaps += 1
-                
-        if not swapped:
-            return swaps
-
-    return swaps
-
-def insertion_sort(arr):
-	for i in range(1, len(arr)):
-		key = arr[i] # this element will be now inserted at it's correct position
-		j = i - 1 # because the right place of insertion will be on left
-		while j >= 0 and key < arr[j]:
-			arr[j+1] = arr[j]
-			j -= 1
-		arr[j+1] = key
-		
-	return
-
-def InsertionSortCustom(li):
-
-    swap_count = 0
-    for j in range(len(li)):
-        key = li[j]
-        i = j
-        while i>=1 and key < li[i-1]:
-            li[i] = li[i-1]
-            swap_count += 1
-            i -= 1
-        li[i] = key
-    return swap_count
-    
-
+ans = 0   
+def Merge(left, right):
+    global ans
+    i, j = 0, 0
+    ans_to_be_returned = []
+    while i < len(left) and j < len(right):
+        if left[i]<right[j]:
+            ans_to_be_returned.append(left[i])
+            i += 1 
+        else:
+            ans_to_be_returned.append(right[j])
+            j += 1
+            ans += len(left) - i 
+    if i == len(left):
+        ans_to_be_returned = ans_to_be_returned + right[j:]
+    else:
+        ans_to_be_returned = ans_to_be_returned + left[i:]
+    return ans_to_be_returned
+         
 # bubble and insertion sort both are giving TLE need something better
-
-
+def MergeSortCustom(arr, l, r):
+    if l == r:
+        return [arr[l]]
+    mid = (l+r)//2
+    left = MergeSortCustom(arr, l, mid)
+    right = MergeSortCustom(arr, mid + 1, r)
+    final_ = Merge(left, right)
+    return final_
+    
 for _ in range(int(input())):
+    ans = 0
     n = int(input())
     li = list(map(int, input().split()))
-    
-    swaps = BubbleSortCustom(li)
-    #print()
-    #insertion_sort(li)
-    #print(li)
-    
-    print(swaps)
+    MergeSortCustom(li, 0, len(li) - 1)
+    print(ans)
