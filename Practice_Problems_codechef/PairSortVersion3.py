a355c2956cf31192f573457ea128848f0b9116b4
Harasees Singh <harasees1202@gmail.com>
2021-03-12 16:56:08 +0530

    ✔

--- Practice_Problems_codechef/PairSortVersion3.py
+++ Practice_Problems_codechef/PairSortVersion3.py
@@ -3,30 +3,26 @@
 2 2 3
 6 3 3
 '''
-def revese(arr):
-    ans = []
-    for i in range(len(arr) - 1, -1, -1):
-        ans.append(arr[i])
-    return ans
-    
+
 n = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 tuples = [(A[i], B[i]) for i in range(n)]
-tuples.sort(key = lambda x:x[1], reverse = True)
+tuples.sort(key = lambda x:x[1], reverse = True)                                # sort according to the second element in the tuple
 
 i = 0
-while i < n-1:
+while i < n-1:  
     count = 0
     j = i
     while j < n-1:
-        if tuples[j][1] == tuples[j+1][1]:
+        if tuples[j][1] == tuples[j+1][1]:                                      # if 2 tuples have the same first element
             count += 1
             j += 1 
         else:
             break
     if count != 0:
-        tuples[(i):(j+1)] = sorted(tuples[(i):(j+1)], key = lambda x:x[0]) 
+        tuples[(i):(j+1)] = sorted(tuples[(i):(j+1)], key = lambda x:x[0])      # slice the list such that the sliced part has all the second elements same, 
+                                                                                # then return a sorted slice (sorted according to the first element) and fit it in the sliced part
 
     i = j+1
     
