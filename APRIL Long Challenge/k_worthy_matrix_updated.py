ccbf50ee09917b46c71654447414989c31630598
Harasees Singh <harasees1202@gmail.com>
2021-04-06 13:14:07 +0530

    partally correct answer✔ 1 case giving tle

--- APRIL Long Challenge/k_worthy_matrix_updated.py
+++ APRIL Long Challenge/k_worthy_matrix_updated.py
@@ -8,22 +8,22 @@
 import copy
 matrix = []
 copy_matrix = []
-
+# 2, 3, 2, 8, 4, 15 wa answer for these parameters-> debug this
 def good(i, j, l, m, n, k):
     
     average_of_matrix_of_side_l = -1
     if l>=2:
         if m-j>=l and n-i>=l:
-            if i+l-1<=m-1 and j-1>= 0:
+            if i+l-1<=n-1 and j-1>= 0:
                 element1 = matrix[i+l-1][j-1]
             else:
                 element1 = 0
-            if j+l-1<=n-1 and i-1>=0:
+            if j+l-1<=m-1 and i-1>=0:
                 element2 = matrix[i-1][j+l-1]
             else:
                 element2 = 0
             if element1 != 0 and element2 != 0:
-                average_of_matrix_of_side_l = (matrix[i+l-1][j+l-1] - element2 - element1 + matrix[0][0])/(l**2)
+                average_of_matrix_of_side_l = (matrix[i+l-1][j+l-1] - element2 - element1 + matrix[i-1][j-1])/(l**2)
             else:
                 average_of_matrix_of_side_l = (matrix[i+l-1][j+l-1] - element2 - element1)/(l**2)
         
@@ -119,6 +119,8 @@ for _ in range(int(input())):
             else:
                 count += m - index - l + 1
             i-=1
+            print(f"indeices start from {index}")
+        print(f"count for {l} = ", count)
         
     print(count)
 
