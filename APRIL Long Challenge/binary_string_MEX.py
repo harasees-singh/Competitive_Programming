3e93ba74ebce684d6964829af5ae1bf5c8a801ab
Harasees Singh <harasees1202@gmail.com>
2021-04-08 15:46:01 +0530

    some minor issue (aiming for subtask 1)

--- APRIL Long Challenge/binary_string_MEX.py
+++ APRIL Long Challenge/binary_string_MEX.py
@@ -1,25 +1,39 @@
-def flip(bit):
-    if bit=='1':
-        return '0'
-    return '1'
-
-
-mid = 0
+def NotASubstring(a, s):
+    i, j = 0, 0
+    while i<len(a) and j<len(s):
+        # if j hits len(s) first then a is not a substring
+        if a[i] == s[j]:
+            i+=1
+            j+=1
+        else:
+            j+=1
+    if j == len(s):
+        return True
+    return False
+    
+    
+from queue import Queue
 for _ in range(int(input())):
     s = input()
-    l = 0
-    r = 2**(10**6)
-    mid = (l+r)//2
-    mid=mid>>1
-    i = len(s)-1
-    while mid!=0 :
-        while i>= 0:
-            bit = mid&1
-            if int(s[i]) == bit:
-                i-=1
-                mid = mid>>1
-            else:
-                i-=1
-        if i == 0:      # we traversed the entire list
-            pass
-    
+    flag = True
+    for i in range(len(s)):
+        if s[i] == "0":
+            flag = False
+    if flag:
+        print(0)
+        break
+    string_len = len(s)
+    n = 2**string_len
+    BinaryQueue = Queue()
+    BinaryQueue.put("1")
+    n = n//2 +1
+    while(n):
+        n-=1
+        string = BinaryQueue.get()
+        if NotASubstring(string, s):
+            print(string)
+            break
+        BinaryQueue.put(string+"0")
+        BinaryQueue.put(string+"1")
+        
+        # first n binary numbers have been generated
