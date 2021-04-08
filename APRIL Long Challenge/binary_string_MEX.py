98674c24fad963c6d86080094716026ab6d5c013
Harasees Singh <harasees1202@gmail.com>
2021-04-08 15:10:43 +0530

    probably a wrong approach

--- APRIL Long Challenge/binary_string_MEX.py
+++ APRIL Long Challenge/binary_string_MEX.py
@@ -3,39 +3,23 @@ def flip(bit):
         return '0'
     return '1'
 
+
+mid = 0
 for _ in range(int(input())):
     s = input()
-    if len(s)>=2:
-        second_bit = s[1]
-        
-        #dp = [0 for _ in range(2000000)]
-        dp ={}
-        dp[0] = '0'
-        if second_bit == '1':
-            dp[1] = '0'
-        else:
-            dp[1] = '11'
-        length = len(s)
-        current_bit  = second_bit
-        for i in range(2, length):
-            if s[i] == current_bit:
-                dp[i] = dp[i-1]
+    l = 0
+    r = 2**(10**6)
+    mid = (l+r)//2
+    mid=mid>>1
+    i = len(s)-1
+    while mid!=0 :
+        while i>= 0:
+            bit = mid&1
+            if int(s[i]) == bit:
+                i-=1
+                mid = mid>>1
             else:
-                if dp[i-2] != '0':
-                    dp[i] = dp[i-2] + flip(s[i])
-                else:
-                    if dp[i-1] == 1:
-                        dp[i] = '1' + dp[i-2] + flip(s[i])
-                    else:
-                        dp[i] = '1' + dp[i-2] + s[i]
-                        #current_bit = '1'
-                        
-            current_bit = s[i]
-        print(dp[length-1])
-    else:
-        if s=='1':
-            print(0)
-        else:
-            print(1)
-
+                i-=1
+        if i == 0:      # we traversed the entire list
+            pass
     
