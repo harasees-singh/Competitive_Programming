65fab381be9660ade8683689835e01c1526f7648
Harasees Singh <harasees1202@gmail.com>
2021-06-05 11:34:25 +0530

    1 liner ✔

---
+++ CODEFORCES ROUND 110 div 2/Q1_teams.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <functional>
+#define infinity 999999999999999999
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define MOD_DEFINE const int MOD = 1e9 + 7;
+#define And &&
+#define Or ||
+#define endl '\n'
+#define space " "
+#define int long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define pb(n) push_back(n)
+#define mii map<int, int>
+#define test_cases_loop int t; cin >> t; while(t--)
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define loop(var, initial, final) for(int var=initial; var < final; var++)
+using namespace std;
+int32_t main(){
+    FIO
+    test_cases_loop{
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if(max(a, b) < min(c, d) Or min(a, b) > max(c, d))cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+}
+
