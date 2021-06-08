bf9333aa0bbb91519e63d816df7e620f298d1105
Harasees Singh <harasees1202@gmail.com>
2021-06-08 16:28:10 +0530

    ✔never ever use doubles and floats; store fractions as pairs

---
+++ CODEFORCES ROUND 724 Div 2/Q3_geometry_and_dp.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <functional>
+#include <numeric>
+#define infinity 999999999999999999
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define MOD_DEFINE const int MOD = 1e9 + 7;
+#define And &&
+#define Or ||
+#define endl '\n'
+#define space " "
+#define int long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define pb(n) push_back(n)
+#define mii map<int, int>
+#define test_cases_loop int t; cin >> t; while(t--)
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define loop(var, initial, final) for(int var=initial; var < final; var++)
+using namespace std;
+int __gcd(int a, int b){
+    if(b==0) return a;
+    return __gcd(b, a%b);
+}
+int32_t main(){
+    FIO;
+    test_cases_loop{
+        map<pii, int> hardware;
+        int n; cin >> n;
+        string s; cin >> s;
+        int d=0, k=0;
+        for(auto a:s){
+            if(a=='D'){d++;} else{k++;}
+            int GCD = __gcd(d, k);
+            pii fraction = {d/GCD, k/GCD};
+            // if(k==0) fraction=infinity; else fraction = d/k;
+            hardware[fraction]++; cout << hardware[fraction] << space;
+        }
+        cout << endl;
+    }
+    return 0;
+}
