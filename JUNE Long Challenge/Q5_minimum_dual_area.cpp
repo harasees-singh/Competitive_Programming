9cb77fc0aaf7cd4f814f67238b36db19c8eda854
Harasees Singh <harasees1202@gmail.com>
2021-06-08 22:22:14 +0530

    ✔

--- JUNE Long Challenge/Q5_minimum_dual_area.cpp
+++ JUNE Long Challenge/Q5_minimum_dual_area.cpp
@@ -41,58 +41,73 @@ int32_t main(){
         int points;
         cin >> points;
         vector<pii> coordinates;
-        vector<int> x_dehydrated, y_dehydrated;
+        // vector<int> x_dehydrated, y_dehydrated;
         loop(i, 0, points){
-            int tempx, tempy;
-            
-            cin >> tempx >> tempy; x_dehydrated.pb(tempx); y_dehydrated.pb(tempy); coordinates.push_back({tempx, tempy});
+            int tempx, tempy;     
+            cin >> tempx >> tempy; coordinates.push_back({tempx, tempy});
         }
-        sort(all(x_dehydrated)); sort(all(y_dehydrated));
-        int diffx=0, diffy=0;
-        int xval=0, yval=0;
-        loop(i, 1, sz(x_dehydrated)){
-            if(x_dehydrated[i] - x_dehydrated[i-1] > diffx) diffx = x_dehydrated[i] - x_dehydrated[i-1], xval = i-1;
-            if(y_dehydrated[i] - y_dehydrated[i-1] > diffy) diffy = y_dehydrated[i] - y_dehydrated[i-1], yval = i-1;
-        }
-        int xrange = 0;
-        int yrange = 0;
-        int xrange2 = 0;
-        int yrange2 = 0;
-        int ymin=coordinates[0].second, ymax=coordinates[0].second;
+        // sort(all(x_dehydrated)); sort(all(y_dehydrated));
+
+        if(points == 1){cout << 0 << endl; continue;}
+
         sort(all(coordinates));
-        loop(x, 0, xval+1){
-            coordinates[x].second < ymin ? ymin = coordinates[x].second : ymin = ymin;
-            coordinates[x].second > ymax ? ymax = coordinates[x].second : ymax = ymax;
+
+        vi x_prefix_min(points); vi x_prefix_max(points); vi x_suffix_min(points); vi x_suffix_max(points);
+
+        int x_min=coordinates[0].second, x_max = coordinates[0].second;
+
+        loop(i, 0, points){
+            int curr = coordinates[i].second;
+            x_min = min(x_min, curr); x_max = max(x_max, curr);
+            x_prefix_max[i] = x_max; x_prefix_min[i] = x_min;
         }
-        yrange = ymax - ymin;
 
-        ymin=coordinates[0].second, ymax=coordinates[0].second;
-        loop(x, xval+1, points){
-            coordinates[x].second < ymin ? ymin = coordinates[x].second : ymin = ymin;
-            coordinates[x].second > ymax ? ymax = coordinates[x].second : ymax = ymax;
+        x_min=coordinates[points-1].second, x_max = coordinates[points-1].second;
+
+        for(int i=points-1; i>=0; i--){
+            int curr = coordinates[i].second;
+            x_min = min(x_min, curr); x_max = max(x_max, curr);
+            x_suffix_max[i] = x_max; x_suffix_min[i] = x_min;
         }
-        yrange2 = ymax - ymin;
+        
+        // everything for x is ready; y min in a domain and y max in a domain;
+        int ansx=infinity;
+        loop(i, 0, points-1){
+            int parition = coordinates[i].first;
+            int next_partition = coordinates[i+1].first;
+            int area = (parition - coordinates[0].first)*(x_prefix_max[i] - x_prefix_min[i]) + (coordinates[points-1].first - next_partition)*(x_suffix_max[i+1] - x_suffix_min[i+1]);
+            ansx = min(area, ansx);
+        }
 
-        int xmin=coordinates[0].first, xmax=coordinates[0].first;
-
+        // -=---------------------------------------------------------------------------------
         sort(all(coordinates), [](pii hell, pii heaven){return hell.second < heaven.second;});
+        // vi x_prefix_min(points); vi x_prefix_max(points); vi x_suffix_min(points); vi x_suffix_max(points);
 
-        // cout << "sorted coordinates " << coordinates[0].second << space << coordinates[1].second << endl;
-        loop(x, 0, yval+1){
-            coordinates[x].first < xmin ? xmin = coordinates[x].first : xmin = xmin;
-            coordinates[x].first > xmax ? xmax = coordinates[x].first : xmax = xmax;
+        x_min=coordinates[0].first, x_max = coordinates[0].first;
+
+        loop(i, 0, points){
+            int curr = coordinates[i].first;
+            x_min = min(x_min, curr); x_max = max(x_max, curr);
+            x_prefix_max[i] = x_max; x_prefix_min[i] = x_min;
         }
-        xrange = ymax-ymin;
 
-        xmin=coordinates[0].first, xmax=coordinates[0].first;
+        x_min=coordinates[points-1].first, x_max = coordinates[points-1].first;
 
-        loop(x, yval+1, points){
-            coordinates[x].first < xmin ? xmin = coordinates[x].first : xmin = xmin;
-            coordinates[x].first > xmax ? xmax = coordinates[x].first : xmax = xmax;
+        for(int i=points-1; i>=0; i--){
+            int curr = coordinates[i].first;
+            x_min = min(x_min, curr); x_max = max(x_max, curr);
+            x_suffix_max[i] = x_max; x_suffix_min[i] = x_min;
         }
-        xrange2 = xmax - xmin;
-        // cout << "ranges " << xrange << space << yrange << endl;
-        cout << min(yrange*(x_dehydrated[xval] - x_dehydrated[0]) + yrange2*(x_dehydrated[points-1] - x_dehydrated[xval+1]), xrange*(y_dehydrated[yval] - y_dehydrated[0]) + xrange2*(y_dehydrated[points-1] - y_dehydrated[yval+1])) << endl;
+        
+        // everything for x is ready; y min in a domain and y max in a domain;
+        int ansy=ansx;
+        loop(i, 0, points-1){
+            int parition = coordinates[i].second;
+            int next_partition = coordinates[i+1].second;
+            int area = (parition - coordinates[0].second)*(x_prefix_max[i] - x_prefix_min[i]) + (coordinates[points-1].second - next_partition)*(x_suffix_max[i+1] - x_suffix_min[i+1]);
+            ansy = min(area, ansy);
+        }
+        cout << ansy << endl;
     }
     return 0;
 }
