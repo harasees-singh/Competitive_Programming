6a0b73f2b35d258638e520f2fbaa3367437f016c
Harasees Singh <harasees1202@gmail.com>
2021-06-08 17:12:28 +0530

    float defined as long double; thanks to wa in cf question due to low precision of float

--- CODEFORCES ROUND 724 Div 2/Q3_retry_float.cpp
+++ CODEFORCES ROUND 724 Div 2/Q3_retry_float.cpp
@@ -18,8 +18,8 @@
 #include <random>
 #include <bitset>
 #include <functional>
+#include <numeric>
 #define infinity 999999999999999999
-#define float long double
 #define sz(v) ((int)(v).size())
 #define all(v) (v).begin(),(v).end()
 #define MOD_DEFINE const int MOD = 1e9 + 7;
@@ -36,7 +36,25 @@
 #define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define loop(var, initial, final) for(int var=initial; var < final; var++)
 using namespace std;
-int32_t main(){
-    FIO
+int __gcd(int a, int b){
+    if(b==0) return a;
+    return __gcd(b, a%b);
 }
-
+int32_t main(){
+    FIO;
+    test_cases_loop{
+        map<long double, int> hardware;
+        int n; cin >> n;
+        string s; cin >> s;
+        long double d=0, k=0;
+        for(auto a:s){
+            if(a=='D'){d++;} else{k++;}
+            long double fraction;
+            k==0 ? fraction=-1 : fraction=d/k;
+            // if(k==0) fraction=infinity; else fraction = d/k;
+            hardware[fraction]++; cout << hardware[fraction] << space;
+        }
+        cout << endl;
+    }
+    return 0;
+}
