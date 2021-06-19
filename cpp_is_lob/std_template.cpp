7d63b2bcbe55b64ecda6f58b7039a55e1525638d
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-06-19 16:00:46 +0530

    updated template ❤❤❤😍

--- cpp_is_lob/std_template.cpp
+++ cpp_is_lob/std_template.cpp
@@ -1,23 +1,4 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <algorithm>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <queue>
-#include <ctime>
-#include <cassert>
-#include <complex>
-#include <string>
-#include <cstring>
-#include <chrono>
-#include <random>
-#include <bitset>
-#include <functional>
+#include<bits/stdc++.h>
 #define infinity 999999999999999999
 #define float long double
 #define sz(v) ((int)(v).size())
@@ -37,7 +18,137 @@
 #define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define loop(var, initial, final) for(int var=initial; var < final; var++)
 using namespace std;
+typedef long long ll;
+template<typename T>
+void print(array<T,2> &arr);
+template<typename T>
+void print(vector<T> &v);
+template<typename T>
+void print(deque<T> &v); 
+template<typename T>
+T _gcd(T a, T b);
+template<typename T>
+vector<T> sieve(T n);
+template<typename T>
+T power(T x, T y, ll p = LLONG_MAX);
+ll nxt();
+// // Returns n^(-1) mod p 
+template<typename T>
+T modInverse(T n, T p);
+ll gcd(vector<int> &diff);
+bool isPowerof2(ll x);
+vector<int> divisors(ll n){
+	vector<int> ans;
+	for(int i = 2;i<=sqrt(n);i++){
+		if(n % i ==0){
+			ans.pb(i);
+			if(n/i != i) ans.pb(n/i);
+		}
+	}
+	ans.pb(1);
+	return ans;
+}
+// ----------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------- //
+
+
+
+// ----------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------- //
+
 int32_t main(){
     FIO
+    
+    return 0;
 }
+
+// ----------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------- //
+
+
+
+// ----------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------------------- //
+template<typename T>
+T power(T x, T y, ll p) { 
+    T res = 1;      // Initialize result 
+  
+    x = x % p;  // Update x if it is more than or 
+                // equal to p 
+    // cout <<"x= "<< x << endl;
+    while (y > 0) 
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res*x) % p; 
+  
+        // y must be even now 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
+    } 
+    // cout << "res" << res << endl;
+    return res; 
+} 
+ 
+template<typename T>
+vector<T> sieve(T n){
+	vector<int> prime(n+1, 1);
+	for(int i = 2; i*i<=n; i++){
+		if(prime[i]){
+			for(int j = 2; j*i<=n; j++){
+				prime[i*j] = 0;
+			}
+		}
+	}
+	vector<T> ans;
+	for(int i = 2; i<=n;i++){
+		if(prime[i]) ans.pb(i);
+	}
+	return prime;
+}
+ 
+template<typename T>
+T _gcd(T a, T b){
+	T temp1 = max(a,b), temp2 = min(a,b);
+	a = temp1, b = temp2;
+	if(a == 0) return b;
+	if(b == 0) return a;
+	if(a%b ==0) return b;
+	return _gcd(b, a%b);
+}
+ 
+template<typename T>
+void print(array<T,2> &arr){
+	cout << arr[0] << " " << arr[1] << endl;
+}
+ 
+template<typename T>
+void print(vector<T> &v){
+	for(T x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+template<typename T>
+T modInverse(T n, T p){
+    return power(n, p-2, p)%p; 
+} 
+ 
+template<typename T>
+void print(deque<T> &v){
+	for(auto x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+ll nxt(){
+    ll x;
+    cin >> x;
+    return x;
+}
+ 
+bool isPowerof2(ll x){
+	return !(x && (x & (x-1)));
+}
+ 
 
