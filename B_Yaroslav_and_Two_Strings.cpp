93b82c8f81709c92977d4739bb3c80ea1fc096f7
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-07-03 12:46:50 +0530

    2000 rated ✔

---
+++ B_Yaroslav_and_Two_Strings.cpp
@@ -0,0 +1,240 @@
+#include<bits/stdc++.h>
+#define infinity 999999999999999999
+#define float long double
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define MOD_DEFINE const int MOD = 1e9 + 7;
+#define And &&
+#define Or ||
+#define endl '\n'
+#define space " "
+#define int long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define pb(n) push_back(n)
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define test_cases_loop int t; cin >> t; while(t--)
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define loop(var, initial, final) for(int var=initial; var < final; var++)
+using namespace std;
+typedef long long ll;
+template<typename T>
+void print(array<T,2> &arr);
+template<typename T>
+void print(vector<T> &v);
+template<typename T>
+void print(deque<T> &v); 
+template<typename T>
+T _gcd(T a, T b);
+template<typename T>
+vector<T> sieve(T n);
+template<typename T>
+T power(T x, T y, ll p = LLONG_MAX);
+ll nxt();
+// // Returns n^(-1) mod p 
+template<typename T>
+T modInverse(T n, T p);
+ll gcd(vector<int> &diff);
+bool isPowerof2(ll x);
+vector<int> divisors(ll n){
+	vector<int> ans;
+	for(int i = 2;i<=sqrt(n);i++){
+		if(n % i ==0){
+			ans.pb(i);
+			if(n/i != i) ans.pb(n/i);
+		}
+	}
+	ans.pb(1);
+	return ans;
+}
+int binomial_cofficient(int n, int r){
+    
+    if(r == 0) return 1;
+    return n*binomial_cofficient(n-1, r-1)/r;
+}
+// ░░░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░░░
+// ░░░░░▄▄▄▄█▀▀▀░░░░░░░░░░░░▀▀██░░░░░░░░░░░
+// ░░▄███▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█▄▄▄░░░░░░░
+// ▄▀▀░█░░░░▀█▄▀▄▀██████░▀█▄▀▄▀████▀░░░░░░░
+// █░░░█░░░░░░▀█▄█▄███▀░░░░▀▀▀▀▀▀▀░▀▀▄░░░░░
+// █░░░█░▄▄▄░░░░░░░░░░░░░░░░░░░░░▀▀░░░█░░░░
+// █░░░▀█░░█░░░░▄░░░░▄░░░░░▀███▀░░░░░░░█░░░
+// █░░░░█░░▀▄░░░░░░▄░░░░░░░░░█░░░░░░░░█▀▄░░
+// ░▀▄▄▀░░░░░▀▀▄▄▄░░░░░░░▄▄▄▀░▀▄▄▄▄▄▀▀░░█░░
+// ░█▄░░░░░░░░░░░░▀▀▀▀▀▀▀░░░░░░░░░░░░░░█░░░
+// ░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄██░░░░
+// ░░▀█▄░░░░░░░░░░░░░░░░░░░░░░░░░▄▀▀░░░▀█░░
+// █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+// █░░█▀▄ █▀▀ █▀█ █░░░░█░▄░█ █ ▀█▀ █░█░░█ ▀█▀░█
+// █░░█░█ █▀▀ █▀█ █░░░░▀▄▀▄▀ █ ░█░ █▀█░░█ ░█░░█
+// █░░▀▀░ ▀▀▀ ▀░▀ ▀▀▀░░░▀░▀░ ▀ ░▀░ ▀░▀░░▀ ░▀░░█
+// ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+int equal(int a, int b){
+    if(a == '?' or b == '?'){
+        if(a == '?' and b == '?') return 10;
+
+        int ans = 1;
+
+        return ans;
+    }
+    return a==b;
+}
+int first_condition(int a, int b){
+    if(a == '?' or b == '?'){
+        if(a == '?' and b == '?') return 9*5;
+
+        int ans;
+
+        a == '?' ? ans = 9 - b + 48: ans = a - 48;
+        return ans;
+    }
+    return a > b;
+}
+int second_condition(int a, int b){
+    if(a == '?' or b == '?'){
+        if(a == '?' and b == '?') return 9*5;
+
+        int ans;
+
+        a == '?' ? ans = b - 48: ans = 9 - a + 48;
+        return ans;
+    }
+    return b > a;
+}
+int all_possibilities(int a, int b){
+    if(a == '?' or b == '?'){
+        if(a == '?' and b == '?') return 10*10;
+
+        int ans = 10;
+
+        return ans;
+    }
+    return 1;
+}
+int32_t main(){
+    MOD_DEFINE;
+    FIO
+    int n; cin >> n;
+    string pehli_string; cin >> pehli_string;
+    string doosri_string; cin >> doosri_string;
+    vector<vi> dp(n+1, vi(4)); dp[0][0] = 1;
+    loop(i, 0, n){
+        int j = i + 1;
+
+        char pehla = pehli_string[i],  doosra = doosri_string[i];
+
+        dp[j][0] = dp[j-1][0]*equal(pehla, doosra);
+
+        dp[j][1] = dp[j-1][1]*(all_possibilities(pehla, doosra) - second_condition(pehla, doosra)) + dp[j-1][0]*first_condition(pehla, doosra); dp[j][1] %= MOD;
+
+        dp[j][2] = dp[j-1][2]*(all_possibilities(pehla, doosra) - first_condition(pehla, doosra)) + dp[j-1][0]*second_condition(pehla, doosra); dp[j][2] %= MOD;
+
+        dp[j][3] = dp[j-1][3]*all_possibilities(pehla, doosra) + dp[j-1][1]*second_condition(pehla, doosra) + dp[j-1][2]*first_condition(pehla, doosra); dp[j][3] %= MOD;
+
+    }
+    cout << dp[n][3] << endl;
+    return 0;
+}
+
+//          ▄              ▄    
+//         ▌▒█           ▄▀▒▌   
+//         ▌▒▒█        ▄▀▒▒▒▐   
+//        ▐▄█▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐   
+//      ▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐   
+//    ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌   
+//   ▐▒▒▒▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▌  
+//   ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐  
+//  ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌ 
+//  ▌░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌ 
+// ▌▒▒▒▄██▄▒▒▒▒▒▒▒▒░░░░░░░░▒▒▒▐ 
+// ▐▒▒▐▄█▄█▌▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+// ▐▒▒▐▀▐▀▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▐ 
+//  ▌▒▒▀▄▄▄▄▄▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▌ 
+//  ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▒▄▒▒▐  
+//   ▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▄▒▒▒▒▌  
+//     ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀   
+//       ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀     
+//          ▀▀▀▀▀▀▀▀▀▀▀▀       
+
+template<typename T>
+T power(T x, T y, ll p) { 
+    T res = 1;      // Initialize result 
+  
+    x = x % p;  // Update x if it is more than or 
+                // equal to p 
+    // cout <<"x= "<< x << endl;
+    while (y > 0) 
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res*x) % p; 
+  
+        // y must be even now 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
+    } 
+    // cout << "res" << res << endl;
+    return res; 
+} 
+ 
+template<typename T>
+vector<T> sieve(T n){
+	vector<int> prime(n+1, 1);
+	for(int i = 2; i*i<=n; i++){
+		if(prime[i]){
+			for(int j = 2; j*i<=n; j++){
+				prime[i*j] = 0;
+			}
+		}
+	}
+	vector<T> ans;
+	for(int i = 2; i<=n;i++){
+		if(prime[i]) ans.pb(i);
+	}
+	return prime;
+}
+ 
+template<typename T>
+T _gcd(T a, T b){
+	T temp1 = max(a,b), temp2 = min(a,b);
+	a = temp1, b = temp2;
+	if(a == 0) return b;
+	if(b == 0) return a;
+	if(a%b ==0) return b;
+	return _gcd(b, a%b);
+}
+ 
+template<typename T>
+void print(array<T,2> &arr){
+	cout << arr[0] << " " << arr[1] << endl;
+}
+ 
+template<typename T>
+void print(vector<T> &v){
+	for(T x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+template<typename T>
+T modInverse(T n, T p){
+    return power(n, p-2, p)%p; 
+} 
+ 
+template<typename T>
+void print(deque<T> &v){
+	for(auto x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+ll nxt(){
+    ll x;
+    cin >> x;
+    return x;
+}
+ 
+bool isPowerof2(ll x){
+	return !(x && (x & (x-1)));
+}
+
