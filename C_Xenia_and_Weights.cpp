704cc88253ce5dc2982ce320f71fd78e748b9e1a
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-07-03 12:47:01 +0530

    1700 rated ✔

---
+++ C_Xenia_and_Weights.cpp
@@ -0,0 +1,278 @@
+#include<bits/stdc++.h>
+#define infinity 999999999999999999
+#define float long double
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define MOD_DEFINE const int MOD = 1e9 + 7;
+#define And &&
+#define Or ||
+#define endl '\n'
+#define space " "
+#define int long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define pb(n) push_back(n)
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define test_cases_loop int t; cin >> t; while(t--)
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define loop(var, initial, final) for(int var=initial; var < final; var++)
+using namespace std;
+typedef long long ll;
+template<typename T>
+void print(array<T,2> &arr);
+template<typename T>
+void print(vector<T> &v);
+template<typename T>
+void print(deque<T> &v); 
+template<typename T>
+T _gcd(T a, T b);
+template<typename T>
+vector<T> sieve(T n);
+template<typename T>
+T power(T x, T y, ll p = LLONG_MAX);
+ll nxt();
+// // Returns n^(-1) mod p 
+template<typename T>
+T modInverse(T n, T p);
+ll gcd(vector<int> &diff);
+bool isPowerof2(ll x);
+vector<int> divisors(ll n){
+	vector<int> ans;
+	for(int i = 2;i<=sqrt(n);i++){
+		if(n % i ==0){
+			ans.pb(i);
+			if(n/i != i) ans.pb(n/i);
+		}
+	}
+	ans.pb(1);
+	return ans;
+}
+int binomial_cofficient(int n, int r){
+    
+    if(r == 0) return 1;
+    return n*binomial_cofficient(n-1, r-1)/r;
+}
+// ░░░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░░░
+// ░░░░░▄▄▄▄█▀▀▀░░░░░░░░░░░░▀▀██░░░░░░░░░░░
+// ░░▄███▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█▄▄▄░░░░░░░
+// ▄▀▀░█░░░░▀█▄▀▄▀██████░▀█▄▀▄▀████▀░░░░░░░
+// █░░░█░░░░░░▀█▄█▄███▀░░░░▀▀▀▀▀▀▀░▀▀▄░░░░░
+// █░░░█░▄▄▄░░░░░░░░░░░░░░░░░░░░░▀▀░░░█░░░░
+// █░░░▀█░░█░░░░▄░░░░▄░░░░░▀███▀░░░░░░░█░░░
+// █░░░░█░░▀▄░░░░░░▄░░░░░░░░░█░░░░░░░░█▀▄░░
+// ░▀▄▄▀░░░░░▀▀▄▄▄░░░░░░░▄▄▄▀░▀▄▄▄▄▄▀▀░░█░░
+// ░█▄░░░░░░░░░░░░▀▀▀▀▀▀▀░░░░░░░░░░░░░░█░░░
+// ░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄██░░░░
+// ░░▀█▄░░░░░░░░░░░░░░░░░░░░░░░░░▄▀▀░░░▀█░░
+// █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+// █░░█▀▄ █▀▀ █▀█ █░░░░█░▄░█ █ ▀█▀ █░█░░█ ▀█▀░█
+// █░░█░█ █▀▀ █▀█ █░░░░▀▄▀▄▀ █ ░█░ █▀█░░█ ░█░░█
+// █░░▀▀░ ▀▀▀ ▀░▀ ▀▀▀░░░▀░▀░ ▀ ░▀░ ▀░▀░░▀ ░▀░░█
+// ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+vi answer;
+
+map<pii, vi> first_second_lara_croft;
+
+map<pii, vi> second_first_lara_croft;
+
+int m;
+
+vi stones;
+
+bool dfs(int move, int curr, int firstsum, int secondsum, int parity){
+
+    if(move == m){
+        return 1;
+    }
+    bool found_answer = false;
+    if(parity){
+        vi empty = {};
+
+        if(first_second_lara_croft[{firstsum, secondsum}] == empty){
+
+            first_second_lara_croft[{firstsum, secondsum}] = stones;
+        }
+        int i = 0;
+
+        for(auto pathar: first_second_lara_croft[{firstsum, secondsum}]){
+            
+            if(pathar <= secondsum - firstsum){
+
+                first_second_lara_croft[{firstsum, secondsum}][i] = 0;
+            }
+
+            else if(pathar != curr){
+                
+                bool ans =  dfs(move + 1, pathar, firstsum+pathar, secondsum, parity^1);
+
+                if(!ans) first_second_lara_croft[{firstsum, secondsum}][i] = 0;
+
+                if(ans) {
+
+                    answer.pb(pathar); found_answer = true; break;
+                }
+
+            }
+
+            i++;
+        }
+    }
+    
+    else{
+
+        vi empty = {};
+
+        if(second_first_lara_croft[{firstsum, secondsum}] == empty){
+
+            second_first_lara_croft[{firstsum, secondsum}] = stones;
+        }
+        int i = 0;
+
+        for(auto pathar: second_first_lara_croft[{firstsum, secondsum}]){
+            
+            if(pathar <= firstsum - secondsum){
+
+                second_first_lara_croft[{firstsum, secondsum}][i] = 0;
+            }
+
+            else if(pathar != curr){
+                
+                bool ans =  dfs(move + 1, pathar, firstsum, secondsum+pathar, parity^1);
+
+                if(!ans) second_first_lara_croft[{firstsum, secondsum}][i] = 0;
+
+                if(ans) {
+
+                    answer.pb(pathar); found_answer = true; break;
+                }
+
+            }
+
+            i++;
+        }
+    }
+    return found_answer;
+}
+int32_t main(){
+    FIO
+    string s; cin >> s;
+    cin >> m;
+    // vi stones;
+    loop(i, 0, 10){
+        if(s[i] == '1') stones.pb(i+1);
+    }
+    if(dfs(0, -1, 0, 0, 1)){
+
+        cout << "YES" << endl;
+
+        reverse(all(answer));
+
+        print<int>(answer);
+    }
+
+    else cout << "NO" << endl;
+
+    return 0;
+}
+
+//          ▄              ▄    
+//         ▌▒█           ▄▀▒▌   
+//         ▌▒▒█        ▄▀▒▒▒▐   
+//        ▐▄█▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐   
+//      ▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐   
+//    ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌   
+//   ▐▒▒▒▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▌  
+//   ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐  
+//  ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌ 
+//  ▌░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌ 
+// ▌▒▒▒▄██▄▒▒▒▒▒▒▒▒░░░░░░░░▒▒▒▐ 
+// ▐▒▒▐▄█▄█▌▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+// ▐▒▒▐▀▐▀▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▐ 
+//  ▌▒▒▀▄▄▄▄▄▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▌ 
+//  ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▒▄▒▒▐  
+//   ▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▄▒▒▒▒▌  
+//     ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀   
+//       ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀     
+//          ▀▀▀▀▀▀▀▀▀▀▀▀       
+
+template<typename T>
+T power(T x, T y, ll p) { 
+    T res = 1;      // Initialize result 
+  
+    x = x % p;  // Update x if it is more than or 
+                // equal to p 
+    // cout <<"x= "<< x << endl;
+    while (y > 0) 
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res*x) % p; 
+  
+        // y must be even now 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
+    } 
+    // cout << "res" << res << endl;
+    return res; 
+} 
+ 
+template<typename T>
+vector<T> sieve(T n){
+	vector<int> prime(n+1, 1);
+	for(int i = 2; i*i<=n; i++){
+		if(prime[i]){
+			for(int j = 2; j*i<=n; j++){
+				prime[i*j] = 0;
+			}
+		}
+	}
+	vector<T> ans;
+	for(int i = 2; i<=n;i++){
+		if(prime[i]) ans.pb(i);
+	}
+	return prime;
+}
+ 
+template<typename T>
+T _gcd(T a, T b){
+	T temp1 = max(a,b), temp2 = min(a,b);
+	a = temp1, b = temp2;
+	if(a == 0) return b;
+	if(b == 0) return a;
+	if(a%b ==0) return b;
+	return _gcd(b, a%b);
+}
+ 
+template<typename T>
+void print(array<T,2> &arr){
+	cout << arr[0] << " " << arr[1] << endl;
+}
+ 
+template<typename T>
+void print(vector<T> &v){
+	for(T x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+template<typename T>
+T modInverse(T n, T p){
+    return power(n, p-2, p)%p; 
+} 
+ 
+template<typename T>
+void print(deque<T> &v){
+	for(auto x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+ll nxt(){
+    ll x;
+    cin >> x;
+    return x;
+}
+ 
+bool isPowerof2(ll x){
+	return !(x && (x & (x-1)));
+}
+
