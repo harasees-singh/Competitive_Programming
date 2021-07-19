436a6aa85d9c1e4170c887648e06316e74feb6dd
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2021-07-19 12:45:14 +0530

    ez ✔🐱‍🚀

---
+++ cpp_is_lob/B_Subsequence_Hate.cpp
@@ -0,0 +1,256 @@
+#include<bits/stdc++.h>
+#define infinity 999999999999999999
+#define float long double
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(),(v).end()
+#define MOD_DEFINE const int MOD = 1e9 + 7;
+#define And &&
+#define Or ||
+#define endl '\n'
+#define space " "
+#define int long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define pb(n) push_back(n)
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define test_cases_loop int t; cin >> t; while(t--)
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define loop(var, initial, final) for(int var=initial; var < final; var++)
+#define cout std::cout
+#define cin std::cin
+
+using namespace std;
+MOD_DEFINE
+typedef long long ll;
+template<typename T>
+void print(array<T,2> &arr);
+template<typename T>
+void print(vector<T> &v);
+template<typename T>
+void print(deque<T> &v); 
+template<typename T>
+T _gcd(T a, T b);
+template<typename T>
+vector<T> sieve(T n);
+template<typename T>
+T power(T x, T y, ll p = LLONG_MAX);
+ll nxt();
+// // Returns n^(-1) mod p 
+template<typename T>
+T modInverse(T n, T p);
+ll gcd(vector<int> &diff);
+bool isPowerof2(ll x);
+vector<int> divisors(ll n){
+	vector<int> ans;
+	for(int i = 2;i<=sqrt(n);i++){
+		if(n % i ==0){
+			ans.pb(i);
+			if(n/i != i) ans.pb(n/i);
+		}
+	}
+	ans.pb(1);
+	return ans;
+}
+int binomial_cofficient(int n, int r){
+    
+    if(r == 0) return 1;
+    return n*binomial_cofficient(n-1, r-1)/r;
+}
+int power_modulus(int x, int y)
+{
+    if (y == 0)
+        return 1;
+
+    else if (y % 2 == 0)
+    {
+        int chloe = power_modulus(x, y/2);
+        
+        return (chloe*chloe)%MOD;
+    }
+    else
+    {
+        int mermaid = power_modulus(x, y/2);
+        
+        return (((mermaid*mermaid)%MOD)*x)%MOD;
+    }
+
+}
+int pow_good(int a, int b){
+    
+    return (int)round(pow(a, b));
+}
+// ░░░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░░░
+// ░░░░░▄▄▄▄█▀▀▀░░░░░░░░░░░░▀▀██░░░░░░░░░░░
+// ░░▄███▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█▄▄▄░░░░░░░
+// ▄▀▀░█░░░░▀█▄▀▄▀██████░▀█▄▀▄▀████▀░░░░░░░
+// █░░░█░░░░░░▀█▄█▄███▀░░░░▀▀▀▀▀▀▀░▀▀▄░░░░░
+// █░░░█░▄▄▄░░░░░░░░░░░░░░░░░░░░░▀▀░░░█░░░░
+// █░░░▀█░░█░░░░▄░░░░▄░░░░░▀███▀░░░░░░░█░░░
+// █░░░░█░░▀▄░░░░░░▄░░░░░░░░░█░░░░░░░░█▀▄░░
+// ░▀▄▄▀░░░░░▀▀▄▄▄░░░░░░░▄▄▄▀░▀▄▄▄▄▄▀▀░░█░░
+// ░█▄░░░░░░░░░░░░▀▀▀▀▀▀▀░░░░░░░░░░░░░░█░░░
+// ░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄██░░░░
+// ░░▀█▄░░░░░░░░░░░░░░░░░░░░░░░░░▄▀▀░░░▀█░░
+// █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+// █░░█▀▄ █▀▀ █▀█ █░░░░█░▄░█ █ ▀█▀ █░█░░█ ▀█▀░█
+// █░░█░█ █▀▀ █▀█ █░░░░▀▄▀▄▀ █ ░█░ █▀█░░█ ░█░░█
+// █░░▀▀░ ▀▀▀ ▀░▀ ▀▀▀░░░▀░▀░ ▀ ░▀░ ▀░▀░░▀ ░▀░░█
+// ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+int32_t main(){
+    FIO
+
+    test_cases_loop{
+
+        string s;
+
+        cin >> s;
+
+        int zeroes = 0, ones = 0;
+
+        s = '+' + s;
+
+        vi onnnnnes_pref(sz(s));
+
+        vi zerrrroes_pref(sz(s));
+
+        loop(i, 1, sz(s)){
+
+            int t = 0;
+
+            s[i] == '1' ? t = 1 : t = 0;
+
+            ones += t, zeroes += !t;
+
+            onnnnnes_pref[i] = onnnnnes_pref[i - 1]  + t;
+
+            zerrrroes_pref[i] = zerrrroes_pref[i - 1] + !t;
+        }
+
+        int minima = infinity;
+
+        loop(i, 0, sz(s)){
+
+            // 1s till first i rest all zeroes;
+
+            int t = zerrrroes_pref[i] + ones - onnnnnes_pref[i];
+
+            if(t < minima) minima = t;
+        }
+
+        loop(i, 0, sz(s)){
+
+            // 0s in the first and then following ones
+
+            int t = onnnnnes_pref[i] + zeroes - zerrrroes_pref[i];
+
+            if(t < minima) minima = t;
+        }
+
+        cout << minima << endl;
+    }
+    
+    return 0;
+}
+
+//          ▄              ▄    
+//         ▌▒█           ▄▀▒▌   
+//         ▌▒▒█        ▄▀▒▒▒▐   
+//        ▐▄█▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐   
+//      ▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐   
+//    ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌   
+//   ▐▒▒▒▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▌  
+//   ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐  
+//  ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌ 
+//  ▌░▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌ 
+// ▌▒▒▒▄██▄▒▒▒▒▒▒▒▒░░░░░░░░▒▒▒▐ 
+// ▐▒▒▐▄█▄█▌▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+// ▐▒▒▐▀▐▀▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▐ 
+//  ▌▒▒▀▄▄▄▄▄▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▒▌ 
+//  ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▒▄▒▒▐  
+//   ▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒▄▒▒▒▒▌  
+//     ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀   
+//       ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀     
+//          ▀▀▀▀▀▀▀▀▀▀▀▀       
+
+template<typename T>
+T power(T x, T y, ll p) { 
+    T res = 1;      // Initialize result 
+  
+    x = x % p;  // Update x if it is more than or 
+                // equal to p 
+    // cout <<"x= "<< x << endl;
+    while (y > 0) 
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res*x) % p; 
+  
+        // y must be even now 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
+    } 
+    // cout << "res" << res << endl;
+    return res; 
+} 
+ 
+template<typename T>
+vector<T> sieve(T n){
+	vector<int> prime(n+1, 1);
+	for(int i = 2; i*i<=n; i++){
+		if(prime[i]){
+			for(int j = 2; j*i<=n; j++){
+				prime[i*j] = 0;
+			}
+		}
+	}
+	vector<T> ans;
+	for(int i = 2; i<=n;i++){
+		if(prime[i]) ans.pb(i);
+	}
+	return prime;
+}
+ 
+template<typename T>
+T _gcd(T a, T b){
+	T temp1 = max(a,b), temp2 = min(a,b);
+	a = temp1, b = temp2;
+	if(a == 0) return b;
+	if(b == 0) return a;
+	if(a%b ==0) return b;
+	return _gcd(b, a%b);
+}
+ 
+template<typename T>
+void print(array<T,2> &arr){
+	cout << arr[0] << " " << arr[1] << endl;
+}
+ 
+template<typename T>
+void print(vector<T> &v){
+	for(T x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+template<typename T>
+T modInverse(T n, T p){
+    return power(n, p-2, p)%p; 
+} 
+ 
+template<typename T>
+void print(deque<T> &v){
+	for(auto x: v) cout << x << " ";
+	cout << endl;
+}
+ 
+ll nxt(){
+    ll x;
+    cin >> x;
+    return x;
+}
+ 
+bool isPowerof2(ll x){
+	return !(x && (x & (x-1)));
+}
+
